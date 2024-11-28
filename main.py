2ba98c448de3199a1efdc2f7b60ce23ae1a2ec0b
ryanb <ryan.bersier@cpnv.ch>
2024-11-28 10:12:48 +0100

    initial pull

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Project : robot for devoteam
+Module : intership test
+Author : Ryan BERSIER
+Start date: 28.11.24
+Latest update: 08.10.24
+Version : 0.1
+"""
+
+# librairy
+from tkinter import *
+
+
+
+
